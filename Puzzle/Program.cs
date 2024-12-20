0b81673835af30db614b1e9bfd95b1ac5e4dae21
Vita <vitahalamay@gmail.com>
2024-12-20 14:47:47 +0200

    Initial commit

---
+++ Puzzle/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Puzzle
+{
+    static class Program
+    {
+        static void Main()
+        {
+            string filePath = "..\\..\\..\\fragments.txt";
+            Console.WriteLine("Loading fragments...");
+            var fragments = LoadFragments(filePath);
+            Console.WriteLine("Starting calculations...");
+            string longestChain = FindLongestChain(fragments);
+            Console.WriteLine("\nCalculations completed.");
+            Console.WriteLine("Longest sequence: " + longestChain);
+        }
+
+        static List<string> LoadFragments(string filePath)
+        {
+            var fragments = new List<string>();
+            foreach (var line in File.ReadLines(filePath))
+            {
+                fragments.Add(line.Trim());
+            }
+            return fragments;
+        }
+
+        static Dictionary<string, List<string>> BuildGraph(List<string> fragments)
+        {
+            var graph = new Dictionary<string, List<string>>();
+            for (int i = 0; i < fragments.Count; i++)
+            {
+                for (int j = 0; j < fragments.Count; j++)
+                {
+                    if (i == j) continue;
+                    string firstEnd = fragments[i].Substring(fragments[i].Length - 2);  // Останні дві цифри першого фрагмента
+                    string secondStart = fragments[j].Substring(0, 2); // Перші дві цифри другого фрагмента
+                    if (firstEnd == secondStart)
+                    {
+                        if (!graph.ContainsKey(fragments[i])) graph[fragments[i]] = new List<string>();
+                        graph[fragments[i]].Add(fragments[j]);
+                    }
+                }
+            }
+            return graph;
+        }
+
+        static string FindLongestChain(List<string> fragments)
+        {
+            var graph = BuildGraph(fragments);
+            string longestChain = string.Empty;
+
+            foreach (var fragment in fragments)
+            {
+                var currentChain = DFS(fragment, graph, new HashSet<string>(), fragment);
+                if (currentChain.Length > longestChain.Length)
+                {
+                    longestChain = currentChain;
+                }
+            }
+
+            return longestChain;
+        }
+
+        static string DFS(string currentFragment, Dictionary<string, List<string>> graph, HashSet<string> visited, string currentChain)
+        {
+            visited.Add(currentFragment);
+            string longest = currentChain;
+
+            if (graph.ContainsKey(currentFragment))
+            {
+                foreach (var neighbor in graph[currentFragment])
+                {
+                    if (!visited.Contains(neighbor))
+                    {
+                        var newChain = DFS(neighbor, graph, new HashSet<string>(visited), currentChain + neighbor.Substring(2)); // Додаємо тільки частину після перших двох цифр
+                        if (newChain.Length > longest.Length)
+                        {
+                            longest = newChain;
+                        }
+                    }
+                }
+            }
+
+            visited.Remove(currentFragment);
+            return longest;
+        }
+    }
+}
